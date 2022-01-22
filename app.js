ab97d4f2d7886501b92cd6bbda827fa900bbea53
SHREYA MALOGI <5hre9a@gmail.com>
2022-01-22 21:46:22 +0530

    version 14 (DB)

--- app.js
+++ app.js
@@ -1,74 +1,140 @@
 //jshint esversion:6
 
-const express = require("express");             //npm init 
-const bodyParser = require("body-parser");      //npm i express body-parser
-const http = require("http");                   //inbuilt module, no need to download
-const date = require(__dirname + "/date.js");   //not npm but own own local module
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
-//the app which was created by using express
+//requiring our mongoose module for our db
+const mongoose = require("mongoose");
+
 const app = express();
 
-//declaring globally but here the old one gets over written if we store as var item = " ";
-// so lets create an array which starts of with these 3 instead of li in ejs
-const items = ["buy food", "cook food", "eat food"];
-const workItems = [];
-
-//EJS code like main() for keeping all the templating html code but naming it as .ejs in views folder
 app.set('view engine', 'ejs');
 
-//for keeping the static files in public folder
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//body parser explicit code to handle inputs in the post req
-app.use(bodyParser.urlencoded({extended:true}));
+//mongodb connection, to avoid deprecation warning
+mongoose.connect("mongodb://localhost:27017/todolistDb", { useNewUrlParser: true });
 
-//get request route
+//mongoose schema
+const itemsSchema = ({
+    name: String
+});
+
+//mongoose model
+const modelItem = mongoose.model("Item", itemsSchema);
+
+//3 new items documents
+const item1 = new modelItem({
+    name: "welcome to your todolist."
+});
+const item2 = new modelItem({
+    name: "hit the + button top add new item."
+});
+const item3 = new modelItem({
+    name: "<-- hit this to delete an item."
+});
+
+//keeping these docs in an array
+const defaultItems = [item1, item2, item3];
+
+//mongoose insertMany()
+modelItem.insertMany(defaultItems, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("success");
+    }
+});
+
+//get route
 app.get("/", function(req, res) {
-//activating our date constant which stores our module getDate, and store this day variable
-//lets call the getDate function which is bound to our date module
-let day = date.getDate();                                   
-    //rendering templates
-    res.render("list", {
-        listTitle : day,
-        newListItems : items,                    
-  });
+    res.render("list", { listTitle: "Today", newListItems: defaultItems }); //it will render all the items (code after db.items.find().pretty()) 
 });
 
+//output in mongoshell will show
+// ---
+// > show dbs
+// admin       0.000GB
+// config      0.000GB
+// local       0.000GB
+// todolistDb  0.000GB
+// > use todolistDb
+// switched to db todolistDb
+// > show collections
+// items
+// > db.items.find().pretty()
+// {
+//         "_id" : ObjectId("61ec26567127ee6980ff072c"),
+//         "name" : "welcome to your todolist.",
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ec26567127ee6980ff072d"),
+//         "name" : "hit the + button top add new item.",
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ec26567127ee6980ff072e"),
+//         "name" : "<-- hit this to delete an item.",
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ec27326b2ddf777b02ad24"),
+//         "name" : "welcome to your todolist.",
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ec27326b2ddf777b02ad25"),
+//         "name" : "hit the + button top add new item.",
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ec27326b2ddf777b02ad26"),
+//         "name" : "<-- hit this to delete an item.",
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ec2bc1cb1dc8b04e89e2b0"),
+//         "name" : "welcome to your todolist.",
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ec2bc1cb1dc8b04e89e2b1"),
+//         "name" : "hit the + button top add new item.",
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ec2bc1cb1dc8b04e89e2b2"),
+//         "name" : "<-- hit this to delete an item.",
+//         "__v" : 0
+// }
+// >
 
-//post request route for home
-app.post("/", function(req, res){
-//passing data from webpage to server by tapping unto the input by   req.body.<input_name>method
-const item =  req.body.newitem;  
-//logic: if the request made by user came from /work route push the elemets to it or else push it to home 
-        if (req.body.list === "work") {
-                workItems.push(item);
-                res.redirect("/work")
-        } else {
+
+//post route
+app.post("/", function(req, res) {
+
+    const item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
         items.push(item);
-              res.redirect('/');   
-        }
-                            
+        res.redirect("/");
+    }
 });
 
-
-//get and post for work route
-
-app.get("/work", function(req,res){
-        res.render("list", {listTitle: "work list", newListItems : workItems});
+app.get("/work", function(req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.post("/work", function(req,res){
-        let items = req.body.newItem;
-        workItems.push(items);
-        res.redirect('/');
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
-
-
-//seting up port
 app.listen(3000, function() {
-    console.log(`server is listening at http://localhost:3000`);
+    console.log("Server started on port 3000");
 });
-
-
-
--- date.js
+++ date.js
@@ -1,70 +1,27 @@
-// okay lol , our date.js is our like our own nodemodule from scratch
-//now to render this we go to app.js and add as packages, but 
-//as it is not an npm package, we append it with __dirname
+//jshint esversion:6
 
-//to export this function to our app.js (dont forget to add s in exports, and dont call the function here)
-//the module.exports is like an object in js which has properties and methods
+exports.getDate = function() {
 
+  const today = new Date();
 
-//to export the get date module (keeping the function in the module and exporting it)
-module.exports.getDate = getDate;
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-//creating a getDate function
-function getDate() {
-    let today = new Date();
+  return today.toLocaleDateString("en-US", options);
 
-    //instead of a switch statemenet, we can create a js object ...ref docs
-    let options = {
-        day:"numeric",
-        weekday: "long",
-        month:"long"
-    };
+};
 
-    //today.toLocaleDateString("en-us") method for dynamically telling operating system time
-    let day = today.toLocaleDateString("en-us", options);
+exports.getDay = function () {
 
-    return day;
+  const today = new Date();
 
-}
+  const options = {
+    weekday: "long"
+  };
 
+  return today.toLocaleDateString("en-US", options);
 
-//to export the get date module (keeping the function in the module and exporting it)
-//instead of module.exports we can also write it as exports. (ref node docs)
-exports.getDay = getDay;
-
-//creating a getDay() function
-function getDay() {
-    const today = new Date();
-    
-    //instead of a switch statemenet, we can create a js object ...ref docs
-    const options = {
-            weekday: "long"    
-    };
-    
-    //today.toLocaleDateString("en-us") method for dynamically telling operating system time
-    const day = today.toLocaleDateString("en-us", options);
-    
-    return day;
-    
-    }
-
-
-
-
-
-// when we console.log("module");  we get output in our console as
-
-// Module {
-//     id: 'C:\\Users\\Shre9\\todolistv1\\date.js',
-//     path: 'C:\\Users\\Shre9\\todolistv1',
-//     exports: {},
-//     filename: 'C:\\Users\\Shre9\\todolistv1\\date.js',
-//     loaded: false,
-//     children: [],
-//     paths: [
-//       'C:\\Users\\Shre9\\todolistv1\\node_modules',
-//       'C:\\Users\\Shre9\\node_modules',
-//       'C:\\Users\\node_modules',
-//       'C:\\node_modules'
-//     ]
-//   }
+};
