04e0e1b70cec8cc3e941b8be5329e3ce16fdc533
SHREYA MALOGI <5hre9a@gmail.com>
2022-01-23 17:59:45 +0530

    version 19 (best)

--- app.js
+++ app.js
@@ -3,22 +3,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-// const ejsLint = require('ejs-lint');
+const _ = require("lodash");
 
 //requiring our mongoose module for our db
 const mongoose = require("mongoose");
 
+//creating our express app
 const app = express();
 
+//ejs view engine
 app.set('view engine', 'ejs');
 
+//body parser code
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//keeping static files in public
 app.use(express.static("public"));
 
 //mongodb connection, to avoid deprecation warning
 mongoose.connect("mongodb://localhost:27017/todolistDb", { useNewUrlParser: true });
 
-//mongoose schema
+//mongoose schema for items
 const itemsSchema = ({
     name: String
 });
@@ -40,56 +45,243 @@ const item3 = new modelItem({
 //keeping these docs in an array
 const defaultItems = [item1, item2, item3];
 
+//mongooose schema for list
+const listSchema = ({
+    name: String,
+    items: [itemsSchema]
+});
 
-//GET READ RENDER
+//list model
+const List = mongoose.model("List", listSchema);
+
+//////////////////////////////////////////////////////////////////////   GET      ////////////////////////////////////////////////////////////////////
+
+//get req for / route
 app.get("/", function(req, res) {
-    modelItem.find({}, function(err, foundItems) { //READ method inside get {} will find all the items, witha call back function along with conditionbs
-        if (foundItems.length === 0) { //if there are currently no iytems then insert trhe default items
-            //mongoose insertMany()
+
+    //Find all in our specified collection
+    modelItem.find({}, function(err, foundItems) {
+
+        if (foundItems.length === 0) {
             modelItem.insertMany(defaultItems, function(err) {
                 if (err) {
                     console.log(err);
                 } else {
-                    console.log("successfully saved default items to db");
+                    console.log("Successfully inserted many items");
                 }
             });
-            res.redirect("/"); ///it will go again to the / route under GET but this time it will fall into the lese block at line 82, because now have items in our items collection and we are able to render the founditems 
+            res.redirect("/");
         } else {
-            res.render("list", { listTitle: "Today", newListItems: foundItems }); //instead of just consolelogging we gonna render directly  //console.log(foundItems); //logging the found items
+            res.render("list", { listTitle: "Today", newListItems: foundItems });
         }
+    })
 
-    });
 });
 
+
+//get for customlist /home, /school etc
+app.get("/:customListName", function(req, res) {
+    const customListName = _.capitalize(req.params.customListName);
+
+    List.findOne({ name: customListName }, function(err, foundList) {
+        if (!err) {
+            if (!foundList) {
+                //Create a new list
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                list.save();
+                res.redirect("/" + customListName);
+            } else {
+                //Show an existing list
+                res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+            }
+        }
+    });
+
+});
+
+
+//////////////////////////////////////////////////////////////////////   POST     ////////////////////////////////////////////////////////////////////
 
 //home route with post
 app.post("/", function(req, res) {
 
-    const itemName = req.body.newItem; //which will refer to the input in the forms when we click the + button to add that new item
+
+    //corresponding to the submit button in forms
+    const itemName = req.body.newItem; //tapping into itemname for user to add that new item
+    const listName = req.body.list; //tapping into listname 
+
+
     //creating a new item document
     const item = new modelItem({
         name: itemName
     });
-    item.save(); //it will save all the 79-83 into our items collection and shows in our database
 
-    res.redirect("/"); //after we save it runs app.get aagain, it willrender the item which got logged to apper on our browser
+
+    //conditions if the listname which got triggerd is today
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");
+    } else {
+        List.findOne({ name: listName }, function(err, foundList) {
+
+            //to pushh a new item into array of items
+            foundList.items.push(item);
+
+            foundList.save();
+            res.redirect("/" + listName) //redirecting to the route where the user came from
+        })
+    }
 
 });
+
+
 
 //home route which deletes with post
 app.post("/delete", function(req, res) {
-    //and the id of the element which was checked i.e 61ed27ad42f8dba1f9d2aead
     const checkedItemId = (req.body.checkbox);
-    //mongoose find by id and remove
-    modelItem.findByIdAndRemove(checkedItemId, function(err) {
-        if (!err) {
-            console.log("we have succesfully deleted checkeditem");
-            res.redirect("/"); //to reflect that on our webpage
-        }
-    })
+    const listName = req.body.listName;
+
+    if (listName === "Today") {
+        modelItem.findByIdAndRemove(checkedItemId, function(err) {
+            if (!err) {
+                console.log("Successfully deleted checked item!");
+                res.redirect("/");
+            }
+        });
+
+    } else {
+        //remove from the existing array along with findoneandupdate mongoose method
+        List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function(err, foundList) {
+            if (!err) {
+                res.redirect("/" + listName);
+            }
+        })
+    }
 });
 
 
+//in db we get
+// > show dbs
+// admin       0.000GB
+// config      0.000GB
+// local       0.000GB
+// todolistDb  0.000GB
+// > use todolistDb
+// switched to db todolistDb
+// > show collections
+// items
+// lists
+// > db.lists.find().pretty()
+// {
+//         "_id" : ObjectId("61ed405cc0bb0f5179d7c800"),
+//         "name" : "home",
+//         "items" : [
+//                 {
+//                         "name" : "welcome to your todolist.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fc")
+//                 },
+//                 {
+//                         "name" : "hit the + button top add new item.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fd")
+//                 },
+//                 {
+//                         "name" : "<-- hit this to delete an item.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fe")
+//                 },
+//                 {
+//                         "name" : "jkykj",
+//                         "_id" : ObjectId("61ed40d5c0bb0f5179d7c89e")
+//                 }
+//         ],
+//         "__v" : 3
+// }
+// {
+//         "_id" : ObjectId("61ed4095c0bb0f5179d7c837"),
+//         "name" : "work",
+//         "items" : [
+//                 {
+//                         "name" : "welcome to your todolist.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fc")
+//                 },
+//                 {
+//                         "name" : "hit the + button top add new item.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fd")
+//                 },
+//                 {
+//                         "name" : "<-- hit this to delete an item.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fe")
+//                 },
+//                 {
+//                         "name" : "lll",
+//                         "_id" : ObjectId("61ed40c9c0bb0f5179d7c88e")
+//                 }
+//         ],
+//         "__v" : 4
+// }
+// {
+//         "_id" : ObjectId("61ed4095c0bb0f5179d7c83d"),
+//         "name" : "work",
+//         "items" : [
+//                 {
+//                         "name" : "welcome to your todolist.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fc")
+//                 },
+//                 {
+//                         "name" : "hit the + button top add new item.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fd")
+//                 },
+//                 {
+//                         "name" : "<-- hit this to delete an item.",
+//                         "_id" : ObjectId("61ed4037c0bb0f5179d7c7fe")
+//                 }
+//         ],
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ed46643d23b2269c99ceea"),
+//         "name" : "Work",
+//         "items" : [
+//                 {
+//                         "name" : "welcome to your todolist.",
+//                         "_id" : ObjectId("61ed465b3d23b2269c99cee5")
+//                 },
+//                 {
+//                         "name" : "hit the + button top add new item.",
+//                         "_id" : ObjectId("61ed465b3d23b2269c99cee6")
+//                 },
+//                 {
+//                         "name" : "<-- hit this to delete an item.",
+//                         "_id" : ObjectId("61ed465b3d23b2269c99cee7")
+//                 }
+//         ],
+//         "__v" : 0
+// }
+// {
+//         "_id" : ObjectId("61ed47383d23b2269c99cef4"),
+//         "name" : "About",
+//         "items" : [
+//                 {
+//                         "name" : "welcome to your todolist.",
+//                         "_id" : ObjectId("61ed465b3d23b2269c99cee5")
+//                 },
+//                 {
+//                         "name" : "hit the + button top add new item.",
+//                         "_id" : ObjectId("61ed465b3d23b2269c99cee6")
+//                 },
+//                 {
+//                         "name" : "<-- hit this to delete an item.",
+//                         "_id" : ObjectId("61ed465b3d23b2269c99cee7")
+//                 }
+//         ],
+//         "__v" : 0
+// }
+// > db.lists.drop()
+// true
+// >
 
 
 
@@ -99,16 +291,6 @@ app.post("/delete", function(req, res) {
 
 
 
-
-
-
-
-
-
-//work route with get
-app.get("/work", function(req, res) {
-    res.render("list", { listTitle: "Work List", newListItems: workItems });
-});
 
 //about route with get
 app.get("/about", function(req, res) {
